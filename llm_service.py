4f99c87c247a830b66a92edbe40e990cbae2a34e
ramprakashpg <rampg23@gmail.com>
2025-04-04 01:23:06 -0400

    Refactor AI agent to retrieve data from pdf and google search

--- llm_service.py
+++ llm_service.py
@@ -1,12 +1,13 @@
-from langchain_community.llms import Ollama
-from langchain.chains import ConversationalRetrievalChain
-from langchain.memory import ConversationBufferMemory
-from langchain.vectorstores import Chroma
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.utilities import WikipediaAPIWrapper
-from bs4 import BeautifulSoup
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 import requests
+from bs4 import BeautifulSoup
+from langchain.chains import ConversationalRetrievalChain
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.utilities import WikipediaAPIWrapper
+from langchain.vectorstores import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.llms import Ollama
 
 llm = Ollama(model="llama3.2")
 memory = ConversationBufferMemory(
@@ -31,17 +32,27 @@ query = "Artificial Intelligence"
 wiki_result = wiki.run(query)
 
 memory_agent_2 = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 # Fetch the webpage
 url = "https://ai.googleblog.com/"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 latest_news = soup.find("h2").text
 
+# Fetch from a PDF file
+loader = PyPDFLoader("assests/ai_book_1.pdf")
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+docs = splitter.split_documents(documents)
+
+# Creating ChromaDB directory
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
-docs = text_splitter.create_documents([wiki_result, latest_news])
+data = text_splitter.create_documents([wiki_result, latest_news])
 
 ai_store = Chroma(persist_directory="./chroma_ai", embedding_function=embedding_model)
 
+ai_store.add_documents(data)
 ai_store.add_documents(docs)
 ai_retriever = ai_store.as_retriever()
 ai_chain = ConversationalRetrievalChain.from_llm(llm, ai_retriever, memory=memory_agent_2,
@@ -54,4 +65,4 @@ def ai_qa(prompt):
 
 response = ai_qa("What is AI?")
 print(response)
-print(ai_qa("What are the uses of it?"))
+print(ai_qa("Explain more about it?"))

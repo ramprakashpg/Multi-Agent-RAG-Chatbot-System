11c5adae69ae854dc052673f2df698cf1bc29286
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-04-06 20:50:53 -0400

    Update chatbotUI.py

--- frontend/chatbotUI.py
+++ frontend/chatbotUI.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 st.set_page_config(page_title="Chatbot", layout="wide")
 st.title("🤖 Adaptive Multi-Agent Chatbot")
 
@@ -18,3 +19,12 @@ chatbot_mode = st.sidebar.selectbox(
     ("general", "ai","concordia"),
     index = 0
 )
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input(f"Ask the '{chatbot_mode}' agent..."):
+    st.session_state.messages.append({"role": "user:", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)

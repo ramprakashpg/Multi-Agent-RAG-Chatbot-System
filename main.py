ebd364a3cdd96570554ecfc5fb8b08a7077ebc57
AswiinR <aswiinr19@gmail.com>
2025-04-08 13:29:31 -0400

    RL integration changes

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from pydantic import BaseModel  # needs to get added in req..
 import ai_llm_service
 import llm_service
 import concordia_llm_service
+import uvicorn
+import general_chat
+import os
 
 app = FastAPI()
 
@@ -28,7 +31,7 @@ async def process_prompt(request_data: ProcessRequestBody):
     user_prompt = request_data.user_prompt
     agent = request_data.agent
     if agent == "general":
-        return {"response": llm_service.general_qa(user_prompt)}
+        return {"response": general_chat.get_response(user_prompt)}
     elif agent == "ai":
         return {"response": ai_llm_service.ai_qa(user_prompt)}
     else:
@@ -39,6 +42,7 @@ async def process_prompt(request_data: ProcessRequestBody):
 async def process_request(feedback_data: FeedbackRequestBody):
     agent_mode = feedback_data.agent
     feedback = feedback_data.feedback
+    general_chat.update_feedback(feedback_data.feedback)
     response = f'Got the feedback {feedback} from {agent_mode} Agent'
     return {"response": response}
 

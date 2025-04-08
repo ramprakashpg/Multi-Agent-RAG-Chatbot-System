2b56c7564ca6fbcf7d05e3c6f47be138322507ec
Sasikiran-31 <kirankumar.2001@yahoo.in>
2025-04-08 00:50:12 -0400

    Configured backend for JSON data

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Query
-
+from pydantic import BaseModel #needs to get added in req..
 import ai_llm_service
 import llm_service
 import uvicorn
@@ -12,16 +12,32 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+class ProcessRequestBody(BaseModel):
+    user_prompt: str
+    agent: str
+
+class FeedbackRequestBody(BaseModel):
+    agent: str
+    feedback: str
+
 
 @app.post("/process/")
-async def process_prompt(prompt: str, parameter: str = Query(...,
-                                                             description="Required processing mode (e.g., ai, general or concordia)")):
-    if parameter == "general":
-        return {"response": llm_service.general_qa(prompt)}
-    elif parameter == "ai":
-        return {"response": ai_llm_service.ai_qa(prompt)}
+async def process_prompt(request_data : ProcessRequestBody):
+    user_prompt = request_data.user_prompt
+    agent = request_data.agent
+    if agent == "general":
+        return {"response": llm_service.general_qa(user_prompt)}
+    elif agent == "ai":
+        return {"response": ai_llm_service.ai_qa(user_prompt)}
     # else:
     #     return{"response": concordia_llm.get_response(prompt)}
+
+@app.post("/feedback/")
+async def process_request(feedback_data : FeedbackRequestBody):
+    agent_mode = feedback_data.agent
+    feedback = feedback_data.feedback
+    response = f'Got the feedback {feedback} from {agent_mode} Agent'
+    return {"response": response}
 
 
 if __name__ == "__main__":
